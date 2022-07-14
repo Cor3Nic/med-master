4d958d391ea93be8858fa9ffae2f88771d6dddef
Michi <michael.hauer99@gmx.at>
2022-07-14 18:19:00 +0200

    First Edition

--- index.js
+++ index.js
@@ -23,15 +23,29 @@ app.get('/consumable', (request, response) => {
 	 });
 });
 
-app.listen(port, () => console.log(`API listening on port ${port}...`))
 
-app.post('/consumables', (request, response)=> {
-	const consumable = request.body.name;
+app.post('/consumables', (request, response)=> {			//Katalog Consumable erstellen
+	const medical_product = [request.body.name,parseInt(request.body.manufacturer_id),request.body.webshop_id,request.body.rd_id]
+	const name = request.body.name;
 	const expires = request.body.expires;
 	const manufacturer_id = request.body.manufacturer_id;
 	const webshop_id = request.body.webshop_id;
 	const rd_id = request.body.rd_id;
-
+	response.send(name);
+	con.query("INSERT INTO medical_product (name, manufacturer_id, webshop_id, rd_id) VALUES (?, ?, ?, ?)", medical_product , function (err, result) {
+		if (err) throw err;
+		console.log(result);
+		console.log("consumable sucessfully created");
+		const consumable = [request.body.expires,result.insertId,];
+		con.query("INSERT INTO consumable (expires,medical_product_id) VALUES (?, ?)", consumable, function (err, result) {
+			if (err) throw err;
+			console.log(result);
+			console.log("consumable sucessfully created");	
+		})
+	}
+	
+	)
+	 
 }
 )
-
+app.listen(port, () => console.log(`API listening on port ${port}...`))
